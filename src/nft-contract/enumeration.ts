85c338cae5706a5c6b9052172ff6ed1a48223044
JewelryKim <jsjs21good@gmail.com>
2022-12-22 12:31:12 +0900

    upgradable

--- src/nft-contract/enumeration.ts
+++ src/nft-contract/enumeration.ts
@@ -7,56 +7,76 @@ import { internalNftToken } from "./nft_core";
 
 //Query for the total supply of NFTs on the contract
 export function internalTotalSupply({
-    contract
-}:{
-    contract: Contract
+  contract,
+}: {
+  contract: Contract;
 }): number {
-    /*
+  /*
         FILL THIS IN
     */
 }
 
 //Query for nft tokens on the contract regardless of the owner using pagination
 export function internalNftTokens({
-    contract,
-    fromIndex,
-    limit
-}:{ 
-    contract: Contract, 
-    fromIndex?: string, 
-    limit?: number
+  contract,
+  fromIndex,
+  limit,
+}: {
+  contract: Contract;
+  fromIndex?: string;
+  limit?: number;
 }): JsonToken[] {
-    /*
+  /*
         FILL THIS IN
     */
 }
 
 //get the total supply of NFTs for a given owner
 export function internalSupplyForOwner({
-    contract,
-    accountId
-}:{
-    contract: Contract, 
-    accountId: string
+  contract,
+  accountId,
+}: {
+  contract: Contract;
+  accountId: string;
 }): number {
-    /*
+  /*
         FILL THIS IN
     */
 }
 
 //Query for all the tokens for an owner
 export function internalTokensForOwner({
-    contract,
-    accountId,
-    fromIndex,
-    limit
-}:{
-    contract: Contract, 
-    accountId: string, 
-    fromIndex?: string, 
-    limit?: number
+  contract,
+  accountId,
+  fromIndex,
+  limit,
+}: {
+  contract: Contract;
+  accountId: string;
+  fromIndex?: string;
+  limit?: number;
 }): JsonToken[] {
-    /*
-        FILL THIS IN
-    */
+  // get the set of tokens for the passed in owner
+  const tokenSet = restoreOwners(contract.tokensPerOwner.get(accountId));
+
+  // if there isn't a set of tokens for the passed in account ID, we'll return 0
+  if (tokenSet == null) {
+    return [];
+  }
+
+  // where to start pagination - if we have a fromIndex, we'll use that otherwise start from 0 index
+  const start = fromIndex ? parseInt(fromIndex) : 0;
+  // take the first "limit" elements in the array. If we didn't specify a limit, use 50
+  const max = limit ? limit : 50;
+
+  const keys = tokenSet.toArray();
+  const tokens: JsonToken[] = [];
+  for (let i = start; i < max; i++) {
+    if (i >= keys.length) {
+      break;
+    }
+    const token = internalNftToken({ contract, tokenId: keys[i] });
+    tokens.push(token);
+  }
+  return tokens;
 }
